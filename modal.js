65ad938cb4e6345b01b52b5b7e4bcfdb7e63c4eb
jojoner16 <laybats.jonathan@orange.fr>
2022-07-07 14:23:00 +0200

    ajout du fichier js en modifiant quelques erreurs et en ajoutant la fermeture de la popup inscription

---
+++ modal.js
@@ -0,0 +1,30 @@
+// DOM Elements
+const modalBg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName("close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalBg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalBg.style.display = "none";
+}
+closeBtn[0].addEventListener("click", closeModal);
+
+// ------ display nav responsive ------ //
+// edit nav
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
